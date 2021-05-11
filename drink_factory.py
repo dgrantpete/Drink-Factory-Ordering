f0113dca3e3e0480dc31a15d8d3e8c57885b8198
dgrantpete <dgrantpete@gmail.com>
2021-05-11 15:01:25 -0600

    Added GUI container function and __str__ method for OrderItem class

--- drink_factory.py
+++ drink_factory.py
@@ -1,6 +1,7 @@
 import os
 import json
 from Dependencies import LabelPrinting, GUI
+
 class OrderItem:
     def __init__(self, base_item, size, add_ins=None):
         if add_ins is None:
@@ -9,6 +10,10 @@ class OrderItem:
         self.size = size
         self.add_ins = add_ins
         self.price = prices[size] + (len(add_ins) * prices["Add-In"])
+
+    def __str__(self):
+        return f'{self.size} {self.base_item} +{", ".join(self.add_ins)} for {self.price}'
+    
     def summary(self):
         summary = f"{self.size}\n{self.base_item}"
         for add_in in self.add_ins:
@@ -21,3 +26,5 @@ with open("menu.json") as menu_file:
     json_data = json.load(menu_file)
     prices = json_data["prices"]
     base_drinks = json_data["base drinks"]
+
+GUI.start_gui()
